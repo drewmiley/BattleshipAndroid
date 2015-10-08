c9f7626637846af7918819193ca292f869728b62
unknown <drewmiley92@gmail.com>
2015-10-08 16:20:22 +0100

    Refactors gridview folder

--- app/src/main/java/com/scottlogic/dmiley/battleship/gridview/DoubleTapListener.java
+++ app/src/main/java/com/scottlogic/dmiley/battleship/gridview/DoubleTapListener.java
@@ -5,9 +5,8 @@ import android.view.MotionEvent;
 
 // Class to listen to motion event on custom view components.
 public class DoubleTapListener extends GestureDetector.SimpleOnGestureListener {
-
-    @Override
-    public boolean onDoubleTap(MotionEvent motionEvent) {
-        return true;
-    }
+  @Override
+  public boolean onDoubleTap(MotionEvent motionEvent) {
+    return true;
+  }
 }
